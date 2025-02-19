6c14a810a29fbe04eeca753a8b589df17c5950a1
JGS-JAVA <wlrncom@naver.com>
2025-02-19 12:50:12 +0900

    cc

---
+++ albaing-api/src/main/java/com/jobjob/albaing/dto/UsersDTO.java
@@ -0,0 +1,8 @@
+package com.jobjob.albaing.dto;
+
+<<<<<<<< HEAD:albaing-api/src/main/java/com/jobjob/albaing/dto/Company.java
+public class Company {
+========
+public class UsersDTO {
+>>>>>>>> d06b39a7a57da23eefd2a4c4412cc5c0aa9be8a9:albaing-api/src/main/java/com/jobjob/albaing/dto/UsersDTO.java
+}
