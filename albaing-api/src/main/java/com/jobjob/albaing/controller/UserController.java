a44a10db55eab73019d24f760cfb4af26ea84beb
JGS-JAVA <wlrncom@naver.com>
2025-02-20 14:08:20 +0900

    유저 서비스, 컨트롤러 작업

--- albaing-api/src/main/java/com/jobjob/albaing/controller/UserController.java
+++ albaing-api/src/main/java/com/jobjob/albaing/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.jobjob.albaing.controller;
 
+import com.jobjob.albaing.dto.User;
+import com.jobjob.albaing.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @PostMapping
+    public void registerUser(@RequestBody User user) {
+        userService.registerUser(user);
+    }
 }
--- albaing-api/src/main/java/com/jobjob/albaing/service/UserService.java
+++ albaing-api/src/main/java/com/jobjob/albaing/service/UserService.java
@@ -1,4 +1,7 @@
 package com.jobjob.albaing.service;
 
+import com.jobjob.albaing.dto.User;
+
 public interface UserService {
+    void registerUser(User user);
 }
