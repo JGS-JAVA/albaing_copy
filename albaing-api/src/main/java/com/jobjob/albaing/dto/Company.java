bae7691a02c75f258f18ff1445a82cf67d36c849
Jiho Park <businessj1ho@gmail.com>
2025-02-20 12:41:02 +0900

    Update Company.java

--- albaing-api/src/main/java/com/jobjob/albaing/dto/Company.java
+++ albaing-api/src/main/java/com/jobjob/albaing/dto/Company.java
@@ -14,8 +14,8 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Company {
-    @Id  // Indicates that this field is the primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-generate values for the primary key (e.g., auto-increment)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long companyId;
 
     private String companyName;
