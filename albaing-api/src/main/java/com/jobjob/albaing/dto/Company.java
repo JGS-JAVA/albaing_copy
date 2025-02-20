caaf08f0622db07544412102688ed31fcdd48a80
JGS-JAVA <wlrncom@naver.com>
2025-02-20 11:44:31 +0900

    컴퍼니, 유저 dto  작성

--- albaing-api/src/main/java/com/jobjob/albaing/dto/Company.java
+++ albaing-api/src/main/java/com/jobjob/albaing/dto/Company.java
@@ -1,4 +1,25 @@
 package com.jobjob.albaing.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
+    private String companyName;
+    private String companyRegistrationNumber;
+    private String companyOwnerName;
+    private String companyOpenDate;
+    private String companyPassword;
+    private String companyEmail;
+    private String companyPhone;
+    private String companyLocalAddress;
+    private String companyApprovalStatus;
+    private String companyCreatedAt;
+    private String companyUpdatedAt;
+    private String companyLogo;
+    private String companyDescription;
+
 }
--- albaing-api/src/main/java/com/jobjob/albaing/dto/User.java
+++ albaing-api/src/main/java/com/jobjob/albaing/dto/User.java
@@ -1,4 +1,24 @@
 package com.jobjob.albaing.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String userEmail;
+    private String userPassword;
+    private String userName;
+    private String userBirthdate;
+    private String userGender;
+    private String userPhone;
+    private String userAddress;
+    private String userProfileImage;
+    private String userCreatedAt;
+    private String userUpdatedAt;
+    private String userTermsAgrrement;
+    private String userIsAdmin;
+
 }
