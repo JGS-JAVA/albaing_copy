e6bc5e895835dcf94cd70b3941a10bb3c4facbe1
coiilee <dakyung96@gmail.com>
2025-02-28 11:37:34 +0900

    myPage user정보 수정/조회 -> resume에서 user로 이동

--- albaing-api/src/main/java/com/jobjob/albaing/service/UserService.java
+++ albaing-api/src/main/java/com/jobjob/albaing/service/UserService.java
@@ -6,5 +6,10 @@ import java.util.Map;
 
 public interface UserService {
 
+    // 사용자 정보 조회
+    User getUserById(int userId);
+
+    // 사용자 정보 수정
+    void updateUser(int userId, String userEmail, String userAddress, String userProfileImage);
 
 }
