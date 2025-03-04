02823a8f3f1c7d91871da68c12d96a684f15d7d1
babtingstar <babtingstar@naver.com>
2025-03-04 17:50:15 +0900

    AdminController

--- albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
+++ albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
@@ -1,4 +1,24 @@
 package com.jobjob.albaing.controller;
 
+import com.jobjob.albaing.dto.User;
+import com.jobjob.albaing.service.AdminServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @GetMapping("/users")
+    private List<User> adminSearchUsers(@PathVariable String userName,
+                                        @PathVariable String password) {}
+
 }
--- albaing-api/src/main/java/com/jobjob/albaing/service/AdminServiceImpl.java
+++ albaing-api/src/main/java/com/jobjob/albaing/service/AdminServiceImpl.java
@@ -3,10 +3,12 @@ package com.jobjob.albaing.service;
 import com.jobjob.albaing.dto.*;
 import com.jobjob.albaing.mapper.AdminMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
@@ -29,16 +31,21 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public List<JobApplication> adminSearchJobApplications(String userName, String companyName, String jobPostTitle) {
         List<JobApplication> adminSearchJobApplications = new ArrayList<>();
-        return List.of();
+        adminSearchJobApplications = adminMapper.adminSearchJobApplications(userName, companyName, jobPostTitle);
+        return adminSearchJobApplications;
     }
 
     @Override
     public List<Company> adminSearchCompanies(String companyName, String companyOwnerName, String companyPhone, String companyRegistrationNumber) {
-        return List.of();
+        List<Company> adminSearchCompanies = new ArrayList<>();
+        adminSearchCompanies = adminMapper.adminSearchCompanies(companyName, companyOwnerName, companyPhone, companyRegistrationNumber);
+        return adminSearchCompanies;
     }
 
     @Override
     public List<JobPost> adminSearchJobPosts(String companyName, String jobPostTitle, String jobPostStatus) {
-        return List.of();
+        List<JobPost> adminSearchJobPosts = new ArrayList<>();
+        adminSearchJobPosts = adminMapper.adminSearchJobPosts(companyName, jobPostTitle, jobPostStatus);
+        return adminSearchJobPosts;
     }
 }
