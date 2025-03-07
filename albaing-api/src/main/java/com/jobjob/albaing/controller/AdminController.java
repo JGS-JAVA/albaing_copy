5a5689e61f49bcf035456258ce077d7a24e375fc
babtingstar <babtingstar@naver.com>
2025-03-07 16:08:27 +0900

    Update AdminController.java

--- albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
+++ albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -37,7 +38,8 @@ public class AdminController {
 
         return adminService.adminSearchResumes(resumeTitle, resumeCategory, resumeJobType, sortOrderBy, isDESC);
     }
-  
+
+    // 지원이력 검색
     @GetMapping("/job-applications")
     public List<JobApplication> adminSearchJobApplications(@PathVariable String userName,
                                                            @PathVariable String companyName,
@@ -45,9 +47,10 @@ public class AdminController {
                                                            @PathVariable String sortOrderBy,
                                                            @PathVariable Boolean isDESC) {
 
-        return adminService.adminSearchJobApplications(userName, companyName, jobPostTitle, sortOrderBy, isDESC);;
+        return adminService.adminSearchJobApplications(userName, companyName, jobPostTitle, sortOrderBy, isDESC);
     }
 
+    // 법인 검색
     @GetMapping("/companies")
     public List<Company> adminSearchCompanies(@PathVariable String companyName,
                                               @PathVariable String companyOwnerName,
@@ -59,6 +62,7 @@ public class AdminController {
         return adminService.adminSearchCompanies(companyName, companyOwnerName, companyPhone, companyRegistrationNumber, sortOrderBy, isDESC);
     }
 
+    // 구인공고 검색
     @GetMapping("/job-posts")
     public List<JobPost> adminSearchJobPosts(@PathVariable String companyName,
                                              @PathVariable String jobPostTitle,
@@ -68,14 +72,43 @@ public class AdminController {
         return adminService.adminSearchJobPosts(companyName, jobPostTitle, jobPostStatus, sortOrderBy, isDESC);
     }
 
+    // 개인 정보 상세 조회
+    @GetMaaping("/users/{userId}")
     public User adminUserDetail(@PathVariable String userId) {
         return adminService.adminUserDetail(userId);
     }
+
 
     public void adminUserDelete(@PathVariable String userId) {
         adminService.adminUserDelete(userId);
     }
 
+    public Resume adminResumeDetail(@PathVariable String resumeId) {
+        return adminService.adminResumeDetail(resumeId);
+    }
 
+    public void adminResumeDelete(@PathVariable String resumeId) {
+        adminService.adminResumeDelete(resumeId);
+    }
+
+    public Company adminCompanyDetail(@PathVariable String companyId) {
+        return adminService.adminCompanyDetail(companyId);
+    }
+
+    public void adminCompanyDelete(@PathVariable String companyId) {
+        adminService.adminCompanyDelete(companyId);
+    }
+
+    public JobPost adminJobPostDetail(@PathVariable String jobPostId) {
+        return adminService.adminJobPostDetail(jobPostId);
+    }
+
+    public void adminJobPostDelete(@PathVariable String jobPostId) {
+        adminService.adminJobPostDelete(jobPostId);
+    }
+
+    public void adminJobPostStatusChange(@PathVariable String companyId) {
+        adminService.adminJobPostStatusChange(companyId);
+    }
 
 }
