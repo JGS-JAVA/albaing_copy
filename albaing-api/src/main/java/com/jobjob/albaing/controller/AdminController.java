4c791c6900bc286694495c5724cd081c066ecd28
babtingstar <babtingstar@naver.com>
2025-03-10 16:22:41 +0900

    Mapper, Controller update and ViewJobApplication new dto

--- albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
+++ albaing-api/src/main/java/com/jobjob/albaing/controller/AdminController.java
@@ -14,106 +14,104 @@ public class AdminController {
     @Autowired
     private AdminServiceImpl adminService;
 
-    // 유저 검색
+    // 개인 검색
     @GetMapping("/users")
-    public List<User> adminSearchUsers(@PathVariable String userName,
-                                       @PathVariable String userEmail,
-                                       @PathVariable String userPhone,
-                                       @PathVariable String sortOrderBy,
-                                       @PathVariable Boolean isDESC) {
+    public List<User> adminSearchUsers(@PathVariable(required = false) String userName,
+                                       @PathVariable(required = false) String userEmail,
+                                       @PathVariable(required = false) String userPhone,
+                                       @PathVariable(required = false) String sortOrderBy,
+                                       @PathVariable(required = false) Boolean isDESC) {
 
         return adminService.adminSearchUsers(userName, userEmail, userPhone, sortOrderBy, isDESC);
     }
 
     // 이력서 검색
     @GetMapping("/resumes")
-    public List<Resume> adminSearchResumes(@PathVariable String resumeTitle,
-                                           @PathVariable String resumeCategory,
-                                           @PathVariable String resumeJobType,
-                                           @PathVariable String sortOrderBy,
-                                           @PathVariable Boolean isDESC) {
+    public List<Resume> adminSearchResumes(@PathVariable(required = false) String resumeTitle,
+                                           @PathVariable(required = false) String resumeCategory,
+                                           @PathVariable(required = false) String resumeJobType,
+                                           @PathVariable(required = false) String sortOrderBy,
+                                           @PathVariable(required = false) Boolean isDESC) {
 
         return adminService.adminSearchResumes(resumeTitle, resumeCategory, resumeJobType, sortOrderBy, isDESC);
     }
 
-    // 지원이력 검색
+    // 공고지원 검색
     @GetMapping("/job-applications")
-    public List<JobApplication> adminSearchJobApplications(@PathVariable String userName,
-                                                           @PathVariable String companyName,
-                                                           @PathVariable String jobPostTitle,
-                                                           @PathVariable String sortOrderBy,
-                                                           @PathVariable Boolean isDESC) {
+    public List<JobApplication> adminSearchJobApplications(@PathVariable(required = false) String userName,
+                                                           @PathVariable(required = false) String companyName,
+                                                           @PathVariable(required = false) String jobPostTitle,
+                                                           @PathVariable(required = false) String sortOrderBy,
+                                                           @PathVariable(required = false) Boolean isDESC) {
 
         return adminService.adminSearchJobApplications(userName, companyName, jobPostTitle, sortOrderBy, isDESC);
     }
 
     // 법인 검색
     @GetMapping("/companies")
-    public List<Company> adminSearchCompanies(@PathVariable String companyName,
-                                              @PathVariable String companyOwnerName,
-                                              @PathVariable String companyPhone,
-                                              @PathVariable String companyRegistrationNumber,
-                                              @PathVariable String sortOrderBy,
-                                              @PathVariable Boolean isDESC) {
+    public List<Company> adminSearchCompanies(@PathVariable(required = false) String companyName,
+                                              @PathVariable(required = false) String companyOwnerName,
+                                              @PathVariable(required = false) String companyPhone,
+                                              @PathVariable(required = false) String companyRegistrationNumber,
+                                              @PathVariable(required = false) String sortOrderBy,
+                                              @PathVariable(required = false) Boolean isDESC) {
 
         return adminService.adminSearchCompanies(companyName, companyOwnerName, companyPhone, companyRegistrationNumber, sortOrderBy, isDESC);
     }
 
-    // 구인공고 검색
+    // 공고 검색
     @GetMapping("/job-posts")
-    public List<JobPost> adminSearchJobPosts(@PathVariable String companyName,
-                                             @PathVariable String jobPostTitle,
-                                             @PathVariable String jobPostStatus,
-                                             @PathVariable String sortOrderBy,
-                                             @PathVariable Boolean isDESC) {
+    public List<JobPost> adminSearchJobPosts(@PathVariable(required = false) String companyName,
+                                             @PathVariable(required = false) String jobPostTitle,
+                                             @PathVariable(required = false) String jobPostStatus,
+                                             @PathVariable(required = false) String sortOrderBy,
+                                             @PathVariable(required = false) Boolean isDESC) {
         return adminService.adminSearchJobPosts(companyName, jobPostTitle, jobPostStatus, sortOrderBy, isDESC);
     }
 
-    // 개인 정보 상세 조회
+    // 개인 상세 조회
     @GetMapping("/users/{userId}")
-    public User adminUserDetail(@PathVariable String userId) {
+    public User adminUserDetail(@PathVariable(required = false) String userId) {
         return adminService.adminUserDetail(userId);
     }
 
+    // 개인 유저 삭제 + 이력서 삭제
     @DeleteMapping("/users/{userId}")
-    public void adminUserDelete(@PathVariable String userId) {
+    public void adminUserDelete(@PathVariable(required = false) String userId) {
         adminService.adminUserDelete(userId);
-    }
-
-    @GetMapping("/resumes/{resumeId}")
-    public Resume adminResumeDetail(@PathVariable String resumeId) {
-        return adminService.adminResumeDetail(resumeId);
-    }
-
-    @DeleteMapping("/resumes/{userId}")
-    public void adminResumeDelete(@PathVariable String userId) {
         adminService.adminResumeDelete(userId);
     }
 
+    // 이력서 상세 조회
+    @GetMapping("/resumes/{resumeId}")
+    public Resume adminResumeDetail(@PathVariable(required = false) String resumeId) {
+        return adminService.adminResumeDetail(resumeId);
+    }
+
+    // 회사 상세 조회
     @GetMapping("/companies/{companyId}")
-    public Company adminCompanyDetail(@PathVariable String companyId) {
+    public Company adminCompanyDetail(@PathVariable(required = false) String companyId) {
         return adminService.adminCompanyDetail(companyId);
     }
 
+    // 회사 유저 삭제 + 공고 상태 전환
     @DeleteMapping("/companies/{companyId}")
-    public void adminCompanyDelete(@PathVariable String companyId) {
+    public void adminCompanyDelete(@PathVariable(required = false) String companyId) {
         adminService.adminCompanyDelete(companyId);
-    }
-
-    @GetMapping("/job-posts/{jobPostId}")
-    public JobPost adminJobPostDetail(@PathVariable String jobPostId) {
-        return adminService.adminJobPostDetail(jobPostId);
-    }
-
-    @DeleteMapping("/job-posts/{jobPostId}")
-    public void adminJobPostDelete(@PathVariable String jobPostId) {
-        adminService.adminJobPostDelete(jobPostId);
-    }
-
-    @PostMapping("/job-posts/status/{companyId}")
-    public void adminJobPostStatusChange(@PathVariable String companyId) {
+        // 공고 상태 비공개로 전환
         adminService.adminJobPostStatusChange(companyId);
     }
 
+    // 공고 상세 조회
+    @GetMapping("/job-posts/{jobPostId}")
+    public JobPost adminJobPostDetail(@PathVariable(required = false) String jobPostId) {
+        return adminService.adminJobPostDetail(jobPostId);
+    }
+
+    // 공고 삭제
+    @DeleteMapping("/job-posts/{jobPostId}")
+    public void adminJobPostDelete(@PathVariable(required = false) String jobPostId) {
+        adminService.adminJobPostDelete(jobPostId);
+    }
 
 }
